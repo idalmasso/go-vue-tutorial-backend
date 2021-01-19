594475341a6fe61387392bff12f8570166fa6e5f
Ivano Dalmasso <ivano.dalmasso@gmail.com>
2021-01-19 14:24:32 +0100

    First commit, removed old not used module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/idalmasso/go-vue-tutorial-backend
+
+go 1.15
+
+require github.com/gorilla/mux v1.8.0
---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main(){
+	r := mux.NewRouter()
+	fs := http.FileServer(http.Dir("./dist"))
+	r.PathPrefix("/").Handler(fs)
+	http.Handle("/",r)
+	fmt.Println("Listening")
+	log.Panic(
+		http.ListenAndServe(":3000", nil),
+	)
+}
